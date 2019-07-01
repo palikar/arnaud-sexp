aaa837c12e097804576a0c59aa34d3f8abd21bbc
Stanislav Arnaudov <stanislav.arnaudov@iosb.fraunhofer.de>
2019-07-01 19:07:43 +0200

    Working on custom brace walker

--- vsexp.el
+++ vsexp.el
@@ -98,46 +98,89 @@
 	(kill-region (region-beginning) (region-end)))
 
 
-(setq bracket-map '(("[" . "]") ("{" . "}")))
-
-(length bracket-map)
-
-(let (value)
-	(dolist (braces bracket-map)
-		(message (car braces))
-		))
-
+(defvar vsexp-bracket-map
+	'(("<" . ">")
+		("(" . ")")
+		("[" . "]")
+		("{" . "}"))
+	"Pair of symbols that are considered a bracket-pair.
+Will fallback to this map if teh sexp is not in the current symbol map")
 
 
-
-(defun xah-get-matching-bracket (@bracket-char-string)
-	"Find the brackets."
-  (interactive)
-  (let (($syntableValue (aref (syntax-table) (string-to-char @bracket-char-string))))
-    (if (or (eq (car $syntableValue ) 4)
+(defun vsexp--get-matching-bracket (bracket-char-string)
+	"Find the other bracket of BRACKET-CHAR-STRING."
+	(interactive)
+	(let (($syntableValue (aref (syntax-table) (string-to-char bracket-char-string))))
+		(if (or (eq (car $syntableValue ) 4)
 						(eq (car $syntableValue ) 5))
-        (char-to-string (cdr $syntableValue))
+				(char-to-string (cdr $syntableValue))
 			(progn
 				(dolist (braces bracket-map)
 					(cond
-					 ((eq (car baces) @bracket-char-string) (cdr baces))
-					 ((eq (cdr baces) @bracket-char-string) (car baces)))
+					 ((string= (car braces) bracket-char-string) (return (cdr braces)))
+					 ((string= (cdr braces) bracket-char-string) (return (car braces)))))))))
+
+(defun vsexp--back-to-brace (char &optional forward)
+  "Go back to CHAR.
+If FORWARD is not nil, the direction is reversed"
+	
+	(let* ((other-char (vsexp--get-matching-bracket char))
+				 (count 0)
+				 (current (char-to-string  (char-after))))
+		(while (or (> count 0) (not (string= current char)))
+
+			(if forward (forward-char 1) (backward-char 1))
+			(while (nth 3 (syntax-ppss)) ((if forward (forward-char 1) (backward-char 1))))
+
+			(setq current (char-to-string  (char-after)))
+			
+			(when (string= current other-char) (setq count (+ count 1)))
+			(when (string= current char) (setq count (- count 1))))))
+
+
+
+(defun vsexp--mark (&optional inside)
+	"Mark inside the thing.
+If INSIDE is not nil, the active region will be around the thing"
+	(let* ((char (char-to-string (read-key "Mark inside:")))
+				 (other-char (vsexp--get-matching-bracket char)))
+		(if other-char
+				(progn
+					
+					(vsexp--back-to char nil)
+					(when inside (forward-char 1))
+					
+					;; (set-mark-command nil)
+					(vsexp--back-to other-char t)
+					;; (unless inside (forward-char 1))
+					
 					)
-				))))
+			(progn
+				
+				(search-backward char nil nil 1)
+				(when inside (forward-char 1))
+				(set-mark-command nil)
+				(forward-char 1)
+				(search-forward char nil nil 1)
+				(when inside (backward-char 1)))
 
+			)))
 
-
-
-(xah-get-matching-bracket "}")
+(defun vsexp-mark-around ()
+	"Mark around char."
+	(interactive)
+	(vsexp--mark nil))
 
 (defun vsexp-mark-inside ()
-	"Mark inside the thing."
+	"Mark inside char."
 	(interactive)
-	(let ((char (read-key "Mark inside:")))
-		
-		
-		(search-backward (char-to-string char) nil nil 1)
-		))
+	(vsexp--mark t))
+
 
 (provide 'vsexp-sexp)
 ;;; vsexp.el ends here
+
+
+
+
+(this is text"{"this is more} this is nothing )
